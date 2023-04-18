adb6e5eb8f42ca1b6900be3af97dff65d8c05d98
JosephCarmeli <josephcarmeli@hotmail.com>
2023-04-18 12:37:01 -0500

    complete

--- app.js
+++ app.js
@@ -7,14 +7,12 @@ let tempEl = document.querySelector(".temp");
 button.addEventListener("click", function() {
   axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=3b198bf6856a18a221a056cee186c32a&units=imperial')
     .then(function (response) {
-      // handle success
       console.log(response);
       nameEl.textContent = response.data.name;
       descEl.textContent = response.data.weather[0].description;
       tempEl.textContent = response.data.main.temp + "°F";
     })
     .catch(function (error) {
-      // handle error
       console.log(error);
     });
 });
